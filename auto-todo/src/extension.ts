269a54f1dd43e01a9e9b04dd848098696f08bf44
Kacper-W-Kozdon <k.kozdon@gmail.com>
2025-10-07 12:48:52 +0200

    adds: placeholder for running auto_todo

--- auto-todo/src/extension.ts
+++ auto-todo/src/extension.ts
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import {exec} from 'child_process';
+
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -19,7 +21,30 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.window.showInformationMessage('Hello World from auto_todo!');
 	});
 
+	const runAutoTodo = vscode.commands.registerCommand('auto-todo.runAutoTodo', () => {
+		const workspaceFolders = vscode.workspace.workspaceFolders;
+		if (!workspaceFolders) {
+			vscode.window.showErrorMessage('No workspace folder is open.');
+			return;
+		}
+
+		const workspacePath = workspaceFolders[0].uri.fsPath;
+
+		exec(`"TODO.py" ${workspacePath}`, { cwd: workspacePath }, (error, stdout, stderr) => {
+			if (error) {
+				vscode.window.showErrorMessage(`Error: ${error.message}`);
+				return;
+			}
+			if (stderr) {
+				vscode.window.showErrorMessage(`stderr: ${stderr}`);
+				return;
+			}
+			vscode.window.showInformationMessage(`auto-todo completed:\n${stdout}`);
+		});
+	});
+
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(runAutoTodo);
 }
 
 // This method is called when your extension is deactivated
