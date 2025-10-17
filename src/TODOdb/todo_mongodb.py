6de5cbeaaa27355788ec8a1dcfdff41c1f607be7
Kacper-W-Kozdon <k.kozdon@gmail.com>
2025-10-17 14:00:20 +0200

    adds: collecting issues inside the database and removing the oldest ones

--- src/TODOdb/todo_mongodb.py
+++ src/TODOdb/todo_mongodb.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 import pathlib
 
@@ -7,34 +8,50 @@ from pymongo import MongoClient
 uri: str = "mongodb://localhost:27017/"
 client: MongoClient = MongoClient(uri)
 auto_todo_db_name = "auto-todo-local"
+max_num_documents = 10
 
 
 def main(project_name: str = "", project_path: str = ".\\TODO.md") -> None:
     database = client["auto-todo-local"]
 
+    now = datetime.datetime.now()
+
+    placeholder_name = "placeholder_name"
     test_path = "\\tests\\TODO.md"
+    issues_collection_name: str = "list-issues"
+
     placeholder_path = f"{pathlib.Path(__file__).parent.parent.resolve()}{test_path}"
 
-    issues_collection_name: str = "list-issues"
     if issues_collection_name not in database.list_collection_names():
         database.create_collection(issues_collection_name)
 
     placeholder_file = open(placeholder_path)
     project_file = open(project_path)
 
-    encoded_file = bson.encode({project_name: placeholder_file.read()})
+    encoded_file = bson.encode({placeholder_name: placeholder_file.read()})
 
     encoded_issues = bson.encode({project_name: project_file.read()})
 
     print(encoded_file)
 
     issues_collection = database.get_collection("list-issues")
-    issues_collection.insert_one({f"{project_name}_issues": encoded_file})
-    issues_collection.insert_one({f"{project_name}_issues": encoded_issues})
+    issues_collection.insert_one(
+        {"time": now, "name": placeholder_name, "issues": encoded_file}
+    )
+    issues_collection.insert_one(
+        {"time": now, "name": project_name, "issues": encoded_issues}
+    )
+
+    while issues_collection.count_documents({}) > max_num_documents:
+        oldest_issues_id = issues_collection.find_one(
+            {"time": {"$exists": "true"}}, sort=[("time", 1)]
+        ).get("id")
+        issues_collection.delete_one({"_id": oldest_issues_id})
 
     with issues_collection.find() as cursor:
         for doc in cursor:
-            print(bson.decode(doc.get(f"{project_name}_issues")))
+            print(bson.decode(doc.get("issues")))
+            print(doc.get("time"))
 
     mongodb_path = pathlib.Path(
         "D:\\Program Files (x86)\\GOG Galaxy\\Games\\Gex"
